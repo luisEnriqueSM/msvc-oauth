ece5ae64e44cae7e24471828a961c6dea028d97b
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2025-03-24 23:32:53 -0600

    Register Eureka Client.

--- src/main/java/com/luis/springcloud/msvc/oauth/MsvcOauthApplication.java
+++ src/main/java/com/luis/springcloud/msvc/oauth/MsvcOauthApplication.java
@@ -1,4 +1,4 @@
-package com.luis.springcloud.msvc.oauth.msvc_oauth;
+package com.luis.springcloud.msvc.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
