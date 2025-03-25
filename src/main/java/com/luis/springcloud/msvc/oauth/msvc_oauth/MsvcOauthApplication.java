dd4b45356ae30bd45b564d2d18924f5f1875634b
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2025-03-24 23:27:32 -0600

    First commit

---
+++ src/main/java/com/luis/springcloud/msvc/oauth/msvc_oauth/MsvcOauthApplication.java
@@ -0,0 +1,13 @@
+package com.luis.springcloud.msvc.oauth.msvc_oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcOauthApplication.class, args);
+	}
+
+}
